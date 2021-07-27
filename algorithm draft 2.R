75fbf839ff62286271d9059ed1acf4888199a6bb
Jakob-Arend <jakob.arend@gmail.com>
2021-07-27 10:47:20 -0500

    Fixing day issue in SLNW algorithm

--- algorithm draft 2.R
+++ algorithm draft 2.R
@@ -1,4 +1,4 @@
-#FULL ALGORITHM ----
+#SET WD AND PATHS ----
 wd <- "C:/Users/User/Desktop/PNC_Lab/activPAL-bout-detection"
 setwd(wd)
 path1 <- "sample_data/SA008-SA008-AP840031 9Apr19 12-00am for 13d 16h 23m-VANE-PB08090417-Events.csv"
@@ -8,8 +8,52 @@ path4 <- "sample_data/SA011-SA011-AP840025 17Apr19 12-00am for 6d 17h 22m-VANE-P
 paths <- c(path1, path2, path3, path4)
 rm(path1, path2, path3, path4)
 
+#HELPER FUNCTIONS FOR LATER ----
+search_forwards <- function(sleep_index, noon_days, data){
+  curr_position <- sleep_index + 1
+  while(curr_position <= nrow(data) && (as.numeric(difftime(data[curr_position, 1], data[sleep_index, 1]), units = "secs") - data[sleep_index, 3]) <= 900){
+    curr_position <- curr_position + 1
+  }
+  forward_SLNW <- c((sleep_index + 1):(curr_position - 1))
+  small_sleep = FALSE
+  for(i in length(forward_SLNW)){
+    if(data[forward_SLNW[i], 3] >= 7200 && data[forward_SLNW[i], 4] < 2){
+      return(forward_SLNW)
+    }else if(data[forward_SLNW[i], 3] >= 1800 && data[forward_SLNW[i], 4] < 2){
+      small_sleep = TRUE
+    }
+  }
+  total_steps <- data[forward_SLNW[length(forward_SLNW)], 5] - data[sleep_index, 5]
+  if((small_sleep && total_steps <= 20) || total_steps == 0){
+    return(forward_SLNW)
+  }
+  return(c())
+}
+
+search_backwards <- function(sleep_index, noon_days, data){
+  curr_position <- sleep_index - 1
+  while(curr_position >= 1 && (as.numeric(difftime(data[sleep_index, 1], data[curr_position, 1]), units = "secs") - data[curr_position, 3]) <= 900){
+    curr_position <- curr_position - 1
+  }
+  backward_SLNW <- c((curr_position + 1):(sleep_index - 1))
+  small_sleep = FALSE
+  for(i in length(backward_SLNW)){
+    if(data[backward_SLNW[i], 3] >= 7200 && data[backward_SLNW[i], 4] < 2){
+      return(backward_SLNW)
+    }else if(data[backward_SLNW[i], 3] >= 1800 && data[backward_SLNW[i], 4] < 2){
+      small_sleep = TRUE
+    }
+  }
+  total_steps <- data[sleep_index, 5] - data[backward_SLNW[1], 5]
+  if((small_sleep && total_steps <= 20) || total_steps == 0){
+    return(backward_SLNW)
+  }
+  return(c())
+}
+
+#WRAPPER LOOP FOR FULL ALGORITHM ----
 for(i in 1:length(paths)){
-  data <- activpalProcessing::activpal.file.reader(paths[2])
+  data <- activpalProcessing::activpal.file.reader(paths[1])
   #USER CONTROLLED OPTIONS ----
   num_days <- 7
   
@@ -66,7 +110,159 @@ for(i in 1:length(paths)){
   }
   rm(curr_day, date, days_to_remove, i, last_day, max, possible_max, start_index)
   
-  #PROCESS DATA FOR SLNW ALGORITHM ----
+  #SLNW ALGORITHM ----
+  curr_day <- paste(valid_days[1], "12:00:00", sep = " ")
+  day_counter <- 1
+  noon_days <- c(1)
+  for(i in 1:nrow(data)){
+    if(as.numeric(difftime(data[i, 1], curr_day), units = "days") > 1) {
+      
+    }
+    if(as.numeric(difftime(data[i, 1], curr_day), units = "secs") > 0) {
+      noon_days <- c(noon_days, i)
+      day_counter <- day_counter + 1
+      if(day_counter > length(valid_days)){
+        break
+      } else {
+        curr_day <- paste(valid_days[day_counter], "12:00:00", sep = " ")
+      }
+    }
+  }
   
+  #To account for the case that our day 7 data never crossed the noon threshold we add a fake index at one past the array
+  if(day_counter == length(valid_days)){
+    noon_days <- c(noon_days, nrow(data) + 1)
+  }
+  #Now we can iterate through each day and identify our sleep periods--this is version B of the SLNW alg parts 1-2
+  all_SLNW <- c()
+  for(i in 1:(length(noon_days) - 1)){
+    sleep_indices = c()
+    longest_bout = 0
+    for(j in noon_days[i]:(noon_days[i + 1] - 1)){
+      if(data[j, 3] >= 18000 && data[j, 4] < 2){
+        sleep_indices <- c(sleep_indices, j)
+      } else if(data[j, 3] >= 7200 && data[j, 4] < 2){
+        longest_bout = j
+      }
+    }
+    if(length(sleep_indices) == 0){
+      sleep_indices <- c(sleep_indices, longest_bout)
+    }
+    for(j in 1:length(sleep_indices)){
+      all_SLNW <- c(all_SLNW, search_backwards(sleep_indices[j], i, noon_days, data), sleep_indices[j], search_forwards(sleep_indices[j], i, noon_days, data))
+    }
+  }
+  all_SLNW <- all_SLNW[!duplicated(all_SLNW)]
+  
+  #Lastly we move these sleep bouts to a new dataframe
+  sleep_data <- data.frame(data[all_SLNW,])
+  data <- data[-c(all_SLNW),]
   
 }
+
+
+
+
+data <- activpalProcessing::activpal.file.reader(paths[2])
+#USER CONTROLLED OPTIONS ----
+num_days <- 7
+
+#REDUCE TO DAY RANGE ----
+curr_day <- as.Date(substr(data[1, 1], 1, 10))
+last_day <- as.Date(substr(data[nrow(data), 1], 1, 10))
+step_counts <- data.frame("remove_me" = 0)
+step_counts[, as.character(curr_day - 1)] <- 0
+step_counts$remove_me <- NULL
+while(as.numeric(difftime(last_day, curr_day)) != -1){
+  step_counts[, as.character(curr_day)] <- 0
+  curr_day <- curr_day + 1
+}
+for(i in 1:nrow(data)){
+  date <- substr(data[i, 1], 1, 10)
+  step_counts[1, date] <- data[i, 5]
+}
+for(i in 2:ncol(step_counts)){
+  if(step_counts[1, i] == 0){
+    step_counts[1, i] = step_counts[1, i-1]
+  }
+}
+for(i in ncol(step_counts):2){
+  step_counts[1, i] <- step_counts[1, i] - step_counts[1, i-1]
+}
+step_counts[, 1] <- NULL
+if(ncol(step_counts) < num_days){
+  print("USER HAS SPECIFIED MORE DAYS THAN ARE AVAILABLE.  REDUCING 'num_days' TO:")
+  print(ncol(step_counts))
+  num_days <- ncol(step_counts)
+}
+start_index <- 0
+max <- 0
+for(i in (ncol(step_counts) - num_days + 1):1){
+  possible_max <- sum(step_counts[,i:(i + num_days - 1)])
+  if(possible_max > max){
+    max <- possible_max
+    start_index <- i
+  }
+}
+valid_days <- c()
+for(i in 0:(num_days - 1)){
+  valid_days <- c(valid_days, colnames(step_counts)[start_index + i])
+}
+step_counts <- step_counts[valid_days]
+days_to_remove <- c()
+for(i in 1:nrow(data)){
+  if(!(substr(data[i, 1], 1, 10) %in% valid_days)){
+    days_to_remove <- c(days_to_remove, i)
+  }
+}
+if(length(days_to_remove) != 0){
+  data <- data[-c(days_to_remove),]
+}
+rm(curr_day, date, days_to_remove, i, last_day, max, possible_max, start_index)
+
+#SLNW ALGORITHM ----
+curr_day <- paste(valid_days[1], "12:00:00", sep = " ")
+noon_days <- c(1)
+for(i in 1:nrow(data)){
+  if(as.numeric(difftime(data[i, 1], curr_day), units = "days") > 1) {
+    
+  }
+  if(as.numeric(difftime(data[i, 1], curr_day), units = "secs") > 0) {
+    noon_days <- c(noon_days, i)
+    if(length(noon_days) > length(valid_days)){
+      break
+    } else {
+      curr_day <- paste(valid_days[length(noon_days)], "12:00:00", sep = " ")
+    }
+  }
+}
+
+#To account for the case that our day 7 data never crossed the noon threshold we add a fake index at one past the array
+if(length(noon_days) == length(valid_days)){
+  noon_days <- c(noon_days, nrow(data) + 1)
+}
+#Now we can iterate through each day and identify our sleep periods--this is version B of the SLNW alg parts 1-2
+all_SLNW <- c()
+for(i in 1:(length(noon_days) - 1)){
+  sleep_indices = c()
+  longest_bout = 0
+  for(j in noon_days[i]:(noon_days[i + 1] - 1)){
+    if(data[j, 3] >= 18000 && data[j, 4] < 2){
+      sleep_indices <- c(sleep_indices, j)
+    } else if(data[j, 3] >= 7200 && data[j, 4] < 2){
+      longest_bout = j
+    }
+  }
+  if(length(sleep_indices) == 0){
+    sleep_indices <- c(sleep_indices, longest_bout)
+  }
+  for(j in 1:length(sleep_indices)){
+    all_SLNW <- c(all_SLNW, search_backwards(sleep_indices[j], noon_days, data), sleep_indices[j], search_forwards(sleep_indices[j], noon_days, data))
+  }
+}
+all_SLNW <- all_SLNW[!duplicated(all_SLNW)]
+
+#Lastly we move these sleep bouts to a new dataframe
+sleep_data <- data.frame(data[all_SLNW,])
+data <- data[-c(all_SLNW),]
+
